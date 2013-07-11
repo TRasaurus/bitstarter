c96374a8adb4363ebfc50b97ec7c06a8330f10f9
TRAsaurus <thomas.j.rico@gmail.com>
2013-07-11 01:50:49 +0000

    filesync1

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("href='https://github.com/TRasaurus/bitstarter/index.html'");
+var fs = require('fs');
+var content = fs.readFileSync('index.html');
+ response.send(content);
 });
 
 var port = process.env.PORT || 5000;
