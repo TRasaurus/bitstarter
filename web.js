3eb7ea58108e7d40e82648aaf13aee06446bd2d7
TRAsaurus <thomas.j.rico@gmail.com>
2013-07-11 02:07:59 +0000

    filesync3

--- web.js
+++ web.js
@@ -15,6 +15,11 @@ app.listen(port, function() {
 var express = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html;), 'utf-8');
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
 });
