ce519f7af9115e7f70a3d1738b96edad52ed9a47
TRAsaurus <thomas.j.rico@gmail.com>
2013-07-13 19:59:27 +0000

    improved web.js

--- web.js
+++ web.js
@@ -1,25 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html"; 
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var content = fs.readFileSync('index.html', 'utf-8');
- response.send(content);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-var express = express.createServer(express.logger());
-app.get('/', function(request, response) {
-var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-response.send(buf.toString());
-});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on " + port);
-});
